70553662b6baec368db1569600e7e5470c8013e9
Ashwinrajkumar <36609731+Ashwinrajkumar@users.noreply.github.com>
2018-04-06 00:36:03 -0400

    Update PluralSight_Restful_API_Script.R

--- PluralSight_Restful_API_Script.R
+++ PluralSight_Restful_API_Script.R
@@ -1,13 +1,13 @@
 
 # Installing the plumber package.
-#install.packages('plumber')
+install.packages('plumber')
 # Loading the plumber package
 library(plumber)
 
 
 # Make sure that the working directory is pointing to the foldr where the R script is located.
 # Pointing to the file and running the endpoints in localhost port = 8000
-r <- plumb("PluralSight.R")  
+r <- plumb("PluralSight_ML_Similarity_Script.R")  
 r$run(port=8000)
 
 
